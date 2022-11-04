1c743c04021b1d6a29593823c48b11f4386ff107
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-11-04 02:23:50 +0200

    Add insights

--- src/routes/insights/+page.svelte
+++ src/routes/insights/+page.svelte
@@ -6,3 +6,62 @@
 		</div>
 	</div>
 </div>
+
+<div class="flex flex-col gap-2 mt-12">
+	<div class="flex gap-2">
+		<div class="bg-slate-800 rounded-lg p-4 grow">
+			<span class="material-symbols-outlined text-slate-300"> sentiment_satisfied </span>
+			<h1 class="text-lg text-slate-300">Sleep welthness</h1>
+			<p>98%</p>
+		</div>
+
+		<div class="flex flex-col grow gap-2">
+			<div class="bg-slate-800 rounded-lg p-4">
+				<span class="material-symbols-outlined text-slate-300"> hourglass_top </span>
+				<div class="flex items-center gap-1">
+					<h1 class="text-lg text-slate-300">Shortest sleep duration</h1>
+				</div>
+
+				<p class="font-semibold text-xl">7 h. 36 min.</p>
+			</div>
+
+			<div class="bg-slate-800 rounded-lg p-4">
+				<span class="material-symbols-outlined text-slate-300"> hourglass_bottom </span>
+				<div class="flex items-center gap-1">
+					<h1 class="text-lg text-slate-300">Longests sleep duration</h1>
+				</div>
+
+				<p class="font-semibold text-xl">9 h. 36 min.</p>
+			</div>
+
+			<div class="bg-slate-800 rounded-lg p-4">
+				<span class="material-symbols-outlined text-slate-300"> hourglass_full </span>
+				<div class="flex items-center gap-1">
+					<h1 class="text-lg text-slate-300">Average sleep duration</h1>
+					
+				</div>
+				<p class="font-semibold text-xl">9 h. 36 min.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="flex gap-2 grow">
+		<div class="bg-slate-800 rounded-lg p-4 grow">
+			<span class="material-symbols-outlined text-slate-300"> bed </span>
+			<div class="flex items-center gap-1">
+				<h1 class="text-lg text-slate-300">Target bed time</h1>
+				<span class="material-symbols-outlined"> info </span>
+			</div>
+			<p class="font-semibold text-xl">9 h. 36 min.</p>
+		</div>
+
+		<div class="bg-slate-800 rounded-lg p-4 grow">
+			<span class="material-symbols-outlined text-slate-300"> alarm </span>
+			<div class="flex items-center gap-1">
+				<h1 class="text-lg text-slate-300">Target wake up time</h1>
+				<span class="material-symbols-outlined"> info </span>
+			</div>
+			<p class="font-semibold text-xl">9 h. 36 min.</p>
+		</div>
+	</div>
+</div>
