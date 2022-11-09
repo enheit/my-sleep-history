7509b14036c975c904dea8de5bd7d2a3763376dd
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-11-09 12:03:07 +0200

    Again add hashRouter

--- src/router/router.config.tsx
+++ src/router/router.config.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { MainLayout } from "../layout/main-layout";
 import { Home } from "../routes/home/home";
 import { NotFound } from "../routes/not-found/not-found";
@@ -6,7 +6,7 @@ import { Welcome } from "../routes/welcome/welcome";
 import { PrivateRoute } from "./private-route";
 import { PublicOnlyRoute } from "./public-only-route";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <MainLayout />,
