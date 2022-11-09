280b1938619c4cb21b87bdbeacf0581abfbf705c
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-11-09 12:02:03 +0200

    Make dark theme as default

--- src/routes/welcome/welcome.jsx
+++ src/routes/welcome/welcome.jsx
@@ -59,7 +59,7 @@ export function Welcome() {
 
                     <div className="flex gap-4">
                         <div className="flex items-center gap-2 text-gray-400 dark:text-gray-800">
-                            <span class="material-symbols-outlined ">
+                            <span className="material-symbols-outlined ">
                                 touch_app
                             </span>
 
@@ -70,7 +70,7 @@ export function Welcome() {
 
             </div>
 
-            <div class="flex items-center items-center justify-between p-4">
+            <div className="flex items-center items-center justify-between p-4">
                 <div className="flex">
                     {/* hover:bg-yellow-500/10 text-yellow-500 */}
                     <button className="flex items-center gap-2 rounded-full px-4 py-2 text-gray-400 dark:text-gray-400" onClick={toggleTheme}>
